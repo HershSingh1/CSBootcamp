1bb9071835f7f2951890e1bcfd2f3507f583a9e1
Hersh Singh <hershvsingh@gmail.com>
2026-07-22 22:34:36 -0500

    contains duplicate done

---
+++ Leetcode/217-containsduplicate.py
@@ -0,0 +1,24 @@
+# time complexity = o(n)
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        numset = set()
+        for num in nums:
+            if num in numset:
+                return True
+            numset.add(num)
+        return False
+
+
+from collections import defaultdict
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        numdict = defaultdict(int)
+        for num in nums:
+            numdict[num] += 1
+            if numdict[num] == 2:
+                return True
+        return False
